726ae0cf8dda2313840d0ad263960d2754c71f4c
Hugofriedrich369 <hungos0223@gmail.com>
2023-03-29 21:26:26 -0300

    [GorjetaAPP] Projeto inicializado

---
+++ app/src/main/java/com/hugoyago/gorjetaapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hugoyago.gorjetaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
