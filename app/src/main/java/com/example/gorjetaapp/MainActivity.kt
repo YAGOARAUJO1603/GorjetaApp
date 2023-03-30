ed3043da8f8e1c07984334082c4fc268caa7de7e
YAGOARAUJO1603 <y.desenvolvedor1603@gmail.com>
2023-03-29 22:42:04 -0300

    FINALIZADO PARTE LÓGICA, ACHAMOS QUE SIM

--- app/src/main/java/com/example/gorjetaapp/MainActivity.kt
+++ app/src/main/java/com/example/gorjetaapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.gorjetaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.gorjetaapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            binding.ButtonCalculate.setOnClickListener{ calculateTip() }
+
+        }
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        val roundUp = binding.switch3.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance()
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
     }
 }
+
