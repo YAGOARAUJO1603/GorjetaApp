d36f6ad6c885a203774c7e759c6f2fadfa2d9fbc
Hugofriedrich369 <hungos0223@gmail.com>
2023-03-29 21:53:55 -0300

    [GorjetaAPP] Projeto Finalizado

--- app/src/main/java/com/hugoyago/gorjetaapp/MainActivity.kt
+++ app/src/main/java/com/hugoyago/gorjetaapp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.hugoyago.gorjetaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.hugoyago.gorjetaapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    fun calculateTip() {
+        val stringInTextField = binding.Cost.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.opcoes.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.amazing -> 0.20
+            R.id.good -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundup.isChecked
+        if (roundUp) {
+            tip = ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.TipAmount.text = getString(R.string.tip_amount, formattedTip)
     }
 }
